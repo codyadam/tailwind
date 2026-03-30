4696b11375954539562be050561d302ab98773fe
Cody Adam <cody.ad4@gmail.com>
2026-03-30 13:17:34 +0200

    fix: restore CMD for server execution in Dockerfile, enabling proper container startup

---
+++ scripts/performance_saver.gd
@@ -0,0 +1,7 @@
+extends Node
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_APPLICATION_FOCUS_IN:
+		Engine.max_fps = 0 #Zero means uncapped
+	if what == NOTIFICATION_APPLICATION_FOCUS_OUT:
+		Engine.max_fps = 1
